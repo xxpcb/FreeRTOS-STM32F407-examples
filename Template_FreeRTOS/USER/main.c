5d4694d82e9276d6fdfd20288f25df149eda5c0f
xxpcb <2260516189@qq.com>
2020-05-30 15:36:03 +0800

    添加STM32F407的Keil工程模板，使用FreeRTOS

---
+++ Template_FreeRTOS/USER/main.c
@@ -0,0 +1,29 @@
+//*******************************
+//STM32F407+FreeRTOS工程模板
+//File: main.c
+//Author: xxpcb(wxgzh:码农爱学习)
+//Version: V1.0
+//Date: 2020/05/30
+//*******************************
+
+#include "stm32f4xx.h"
+#include "led.h"
+#include "delay.h"
+
+int main(void)
+{
+	LED_Init();
+	delay_init(84);
+	
+	while(1)
+	{
+		//LEDa亮500ms,灭500ms
+		LEDa_ON;
+		delay_ms(500);
+		LEDa_OFF;
+		delay_ms(500);
+
+		//LEDb每隔1000ms翻转一次
+		LEDb_Toggle;
+	}
+}
